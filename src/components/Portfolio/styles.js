97c4adf207a9ee1cfdcaa33419fb2d9ba050e796
Zackrey Bichsel <biksel09@gmail.com>
2023-09-15 22:15:27 -0500

    updating portfolio page

--- src/components/Portfolio/styles.js
+++ src/components/Portfolio/styles.js
@@ -1,8 +1,8 @@
-export const mainWrapper = "flex flex-wrap gap-6 justify-evenly items-center";
+export const mainWrapper = "bg-[#FAF8FB] flex flex-wrap gap-6 justify-evenly items-center overflow-visible overflow-y-scroll";
 
 // !===!
 
-export const cardWrapper = "relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md";
+export const cardWrapper = "relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md mt-3";
 
 // !===!
 
