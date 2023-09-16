20abe1c611b9250cd009f74a9e069977ca17e57f
Zackrey Bichsel <biksel09@gmail.com>
2023-09-16 15:10:12 -0500

    added phone

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -50,7 +50,7 @@ export default function Footer() {
                         </a>
                         <div className={div5}>
                             <p className={p1}>
-                            "I swore never to be silent whenever and wherever human beings endure suffering and humiliation. We must always take sides. Neutrality helps the oppressor, never the victim. Silence encourages the tormentor, never the tormented." — Elie Wiesel
+                                "I swore never to be silent whenever and wherever human beings endure suffering and humiliation. We must always take sides. Neutrality helps the oppressor, never the victim. Silence encourages the tormentor, never the tormented." — Elie Wiesel
                             </p>
 
                         </div>
@@ -84,8 +84,16 @@ export default function Footer() {
                             href="https://github.com/zbichsel"
                             className={a2}
                         >
-                            <svg className="h-5 text-gray-800 dark:text-white" fill="currentColor" viewBox="0 0 20 20">
+                            <svg className="h-5 text-zinc-50" fill="currentColor" viewBox="0 0 20 20">
                                 <path fillRule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clipRule="evenodd" />
+                            </svg>
+                        </a>
+                        <a
+                            href="tel:+12142065006"
+                            className={a2}
+                        >
+                            <svg className="h-5 text-zinc-50" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 14h12M1 4h12M6.5 16.5h1M2 1h10a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1Z" />
                             </svg>
                         </a>
                     </div>
