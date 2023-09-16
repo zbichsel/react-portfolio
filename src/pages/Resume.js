096492716b23180562c6720df328fcb712f2bfc8
Zackrey Bichsel <biksel09@gmail.com>
2023-09-16 14:12:57 -0500

    starting resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import Header from '../components/Header';
 import Nav from '../components/Nav';
 import Resume from '../components/Resume';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 
 
 export default function Resumepage() {
     return (
         <>
-            <Nav/>
-            <Resume/>
+            <Header />
+            <Nav />
+            <Resume />
+            <Contact />
+            <Footer />
         </>
     );
 }
