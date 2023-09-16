bf8e25fd93a343d967d5a5d66e8a17587090668d
Zackrey Bichsel <biksel09@gmail.com>
2023-09-15 21:47:19 -0500

    added svgs to open and close mobile menu

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,33 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../images/sixFK901.svg';
-import { navWrapper, divWrapper, itemsdiv, itemsul, items } from './styles';
+import { navWrapper, divWrapper, btnDiv, btnNav, svg1, svg2, itemsdiv, itemsul, items } from './styles';
 
 
 function Nav() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
         <>
             <nav className={navWrapper}>
                 <div className={divWrapper}>
-                <div className={itemsdiv}>
-                    <ul className={itemsul}>
-                        <li>
-                            <Link to="/" className={items}>Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about" className={items}>About</Link>
-                        </li>
-                        <li>
-                            <Link to="/portfolio" className={items}>Portfolio</Link>
-                        </li>
-                        <li>
-                            <Link to="/resume" className={items}>Resume</Link>
-                        </li>
-                    </ul>
+                    <div className={btnDiv}>
+                        <button className={btnNav} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                            <svg
+                                className={`${svg1} ${isMenuOpen ? "hidden" : "block"}`}
+                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14"
+                            >
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M1 1h15M1 7h15M1 13h15" />
+                            </svg>
+                            <svg
+                                className={`${svg2} ${isMenuOpen ? "block" : "hidden"}`}
+                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14"
+                            >
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div className={`${itemsdiv} ${isMenuOpen ? "block" : "hidden"}`}>
+                        <ul className={itemsul}>
+                            <li>
+                                <Link to="/" className={items}>Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about" className={items}>About</Link>
+                            </li>
+                            <li>
+                                <Link to="/portfolio" className={items}>Portfolio</Link>
+                            </li>
+                            <li>
+                                <Link to="/resume" className={items}>Resume</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav >
+            </nav >
         </>
     );
 }
--- src/components/Nav/styles.js
+++ src/components/Nav/styles.js
@@ -2,11 +2,27 @@ export const navWrapper = "bg-[#C0A6BD] border-b-2 border-black";
 
 // !===!
 
-export const divWrapper = "max-w-screen-xl flex items-center justify-end mx-auto p-4";
+export const divWrapper = "max-w-screen-xl flex items-center justify-center mx-auto p-4";
 
 // !===!
 
-export const itemsdiv = "flex justify-center items-center w-full md:block md:w-auto";
+export const btnDiv = "block md:hidden";
+
+// !===!
+
+export const btnNav = "flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400";
+
+// !===!
+
+export const svg1 = "fill-white h-10 w-10 border p-1 rounded-md";
+
+// !===!
+
+export const svg2 = "fill-current h-10 w-10 border p-1 rounded-md ";
+
+// !===!
+
+export const itemsdiv = "block flex justify-center items-center w-full md:block md:w-auto";
 
 // !===!
 
