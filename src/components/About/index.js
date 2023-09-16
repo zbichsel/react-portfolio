1846090693e77e05befaadbf0cc6315629638320
Zackrey Bichsel <biksel09@gmail.com>
2023-09-16 14:32:09 -0500

    cleaning up images

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { divWrapper, divP, imgDiv, imgDimens, imgCard, cardWrapper, imgProps } from './styles';
+import { divWrapper, divP, imgCard, cardWrapper, imgProps } from './styles';
 import logo from '../../images/zack.jpeg';
 
 
@@ -8,9 +8,7 @@ export default function About() {
         <div className={divWrapper} id="about">
             <div className={cardWrapper}>
                 <div className={imgCard}>
-                    <a href="https://liq543.github.io/Park-Safe/">
-                        <img className={imgProps} src={logo} alt="Park Safe Banner" />
-                    </a>
+                    <img className={imgProps} src={logo} alt="a man standing at the grassy knoll" />
                 </div>
             </div>
             <p className={divP}>I have always been deeply passionate about design and web development, and this ardor has grown into a true love affair over the years. The creative freedom, problem-solving challenges, and the endless possibilities of crafting digital experiences have always fascinated me. Recently, I took a significant step towards realizing my dreams by successfully graduating from a rigorous full stack web development bootcamp. This transformative experience not only honed my technical skills but also deepened my appreciation for the art of creating beautiful, functional, and user-friendly websites and applications. I am now more excited than ever to embark on a journey where I can combine my passion for design with my newfound expertise in web development to craft innovative and visually captivating digital solutions.</p>
