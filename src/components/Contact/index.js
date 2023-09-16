ff16da050f8a46403b320749bdb9aa9c7ac11869
Zackrey Bichsel <biksel09@gmail.com>
2023-09-16 13:55:36 -0500

    updated contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,43 +1,48 @@
 import React from 'react';
+import { btn, divWrapper, divWrapper2, divWrapper3, divWrapper4, divWrapper5, divWrapper6, header, imgWrp, input1, input2, jacko, span, textrea, } from './styles';
+import logo from '../../images/5875388.jpg';
 
 export default function Contact() {
     return (
-        <div className="bg-[#FAF8FB] antialiased font-nav px-8 py-12" id="contact">
-            <div className='text-center w-full'>
+        <div className={divWrapper} id="contact">
+            <div className={divWrapper2}>
                 {/* svg supposed to be here */}
             </div>
-            <div className='max-w-screen-xl px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-zinc-50 text-gray-900 rounded-xl shadow-md'>
-                <div className='flex flex-col justify-between'>
+            <div className={divWrapper3}>
+                <div className={divWrapper4}>
                     <div>
-                        <h2 className='text-4xl lg:text-5xl font-bold leading-tight'>Lets talk about something</h2>
-                        <div className='text-gray-700 mt-8'>
+                        <h2 className={header}>Lets talk about something</h2>
+                        <div className={divWrapper5}>
                             Hate Forms?
                             Send us an <span className='underline'>Email</span> instead.
                         </div>
                     </div>
-                    <div className='mt-8 text-center'>
+                    <div className={divWrapper6}>
                         {/* svg supposed to be */}
                     </div>
+                        <div className={imgWrp}>
+                        <img src={logo} className={jacko} alt="jackolantern" />
+                        </div>
                 </div>
-                <div className=''>
+                <div>
                     <div>
                         <span
-                            className='uppercase text-sm text-[#C0A6BD] font-semibold'>Full Name</span>
+                            className={span}>Full Name</span>
                             <input
-                                className='w-full bg-[#C0A6BD] text-gray-900 placeholder-zinc-50 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline'
+                                className={input1}
                                 type="text"
                                 placeholder='Michael Audrey Myers'/>
                     </div>
                     <div className='mt-8'>
-                        <span className='uppercase text-sm text-[#C0A6BD] font-semibold'>Email</span>
-                        <input className='w-full bg-[#C0A6BD] text-gray-900 placeholder-zinc-50 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline' type="text" placeholder='mmyers@haddonfield.com'/>
+                        <span className={span}>Email</span>
+                        <input className={input2} type="text" placeholder='mmyers@haddonfield.com'/>
                     </div>
                     <div className='mt-8'>
-                        <span className='uppercase text-sm text-[#C0A6BD] font-semibold'>Message</span>
-                        <textarea className='w-full h-32 bg-[#C0A6BD] text-gray-900 placeholder-zinc-50 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline' placeholder='your text here'></textarea>
+                        <span className={span}>Message</span>
+                        <textarea className={textrea} placeholder='your text here'></textarea>
                     </div>
                     <div className='mt-8'>
-                        <button className='text-[#D242CF] uppercase hover:text-zinc-50 border border-[#D242CF] hover:bg-[#D242CF] focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium leading-snug rounded-lg text-md px-5 py-2.5 text-center mr-2 mb-2 w-full'>Send Message</button>
+                        <button className={btn}>Send Message</button>
                     </div>
                 </div>
             </div>
---
+++ src/components/Contact/styles.js
@@ -0,0 +1,53 @@
+export const divWrapper = "bg-[#FAF8FB] antialiased font-nav px-1 md:px-8 py-12";
+
+// !===!
+
+export const divWrapper2 = "text-center w-full";
+
+// !===!
+
+export const divWrapper3 = "max-w-screen-xl  px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-zinc-50 text-gray-900 rounded-xl shadow-md";
+
+// !===!
+
+export const divWrapper4 = "flex flex-col justify-between";
+
+// !===!
+
+export const header = "text-4xl lg:text-5xl font-bold leading-tight";
+
+// !===!
+
+export const divWrapper5 = "text-gray-700 mt-8";
+
+// !===!
+
+export const divWrapper6 = "mt-8 text-center";
+
+// !===!
+
+export const span = "uppercase text-sm text-[#C0A6BD] font-semibold";
+
+// !===!
+
+export const input1 = "w-full bg-[#C0A6BD] text-gray-900 placeholder-zinc-50 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline";
+
+// !===!
+
+export const textrea = "w-full h-32 bg-[#C0A6BD] text-gray-900 placeholder-zinc-50 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline";
+
+// !===!
+
+export const input2 = "w-full bg-[#C0A6BD] text-gray-900 placeholder-zinc-50 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline";
+
+// !===!
+
+export const btn = "text-[#D242CF] uppercase hover:text-zinc-50 border border-[#D242CF] hover:bg-[#D242CF] focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium leading-snug rounded-lg text-md px-5 py-2.5 text-center mr-2 mb-2 w-full";
+
+// !===!
+
+export const imgWrp = "mx-auto md:m-0 pt-2 pl-1 pb-1 pr-0 rounded-lg flex justify-center items-center shadow-md h-56 w-56";
+
+// !===!
+
+export const jacko = "w-48 rounded-lg shadow-lg";
