59e1dd06d1b39047f02e166659fa32b249412841
Zackrey Bichsel <biksel09@gmail.com>
2023-09-17 20:59:36 -0500

    debugging 2

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
+const uri = process.env.MONGODB_URI;
+
 mongoose.connect(uri, { useUnifiedTopology: true, useNewParser: true })
 .then(() => console.log('MongoDB connected!'))
 .catch(err => console.log('Error:- ' + err));
