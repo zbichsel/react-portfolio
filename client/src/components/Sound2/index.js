af0341334612004477654228f1663d2a6d243384
Zackrey Bichsel <biksel09@gmail.com>
2024-09-25 15:30:35 -0500

    heroku build size

--- client/src/components/Sound2/index.js
+++ client/src/components/Sound2/index.js
@@ -1,29 +1,21 @@
 import React, { useState } from 'react';
 import ReactHowler from 'react-howler';
-import SongOne from '../../sounds/Where is Jeremy.wav';
 import SongTwo from '../../sounds/Halloween Ends Main Title.wav';
-import SongThree from '../../sounds/Lauries Theme Ends.wav';
 import SongSeven from '../../sounds/Evil Eyes.wav';
 import SongEight from '../../sounds/Transformation.wav';
 import SongTwelve from '../../sounds/Corey and Michael.wav';
 import SongFourteen from '../../sounds/The Junk Yard.wav';
 import SongSixteen from '../../sounds/Bye Bye Corey.wav';
-import SongSeventeen from '../../sounds/The Flight.wav';
-import SongEighteen from '../../sounds/Before Her Eyes.wav';
 import SongNineteen from '../../sounds/The Procession.wav';
 
 
 const songs = [
-    { id: 'SongOne', title: 'Where is Jeremy', src: SongOne },
     { id: 'SongTwo', title: 'Halloween Ends Main Title', src: SongTwo },
-    { id: 'SongThree', title: 'Lauries Theme Ends', src: SongThree },
     { id: 'SongSeven', title: 'Evil Eyes', src: SongSeven },
     { id: 'SongEight', title: 'Transformation', src: SongEight },
     { id: 'SongTwelve', title: 'Corey and Michael', src: SongTwelve },
     { id: 'SongFourteen', title: 'The Junk Yard', src: SongFourteen },
     { id: 'SongSixteen', title: 'Bye Bye Corey', src: SongSixteen },
-    { id: 'SongSeventeen', title: 'The Flight', src: SongSeventeen },
-    { id: 'SongEighteen', title: 'Before Her Eyes', src: SongEighteen },
     { id: 'SongNineteen', title: 'The Procession', src: SongNineteen }
 ];
 
